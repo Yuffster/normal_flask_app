fda50f0b2c7449c9e252419e400b504d30d7b04c
Michelle Steigerwalt <michelle@opentrons.com>
2016-04-21 13:29:58 -0400

    PageHandlers start at the bottom and stop after first handler.

--- app/assets/scripts/application.js
+++ app/assets/scripts/application.js
@@ -10,21 +10,26 @@ function Application() {
 	function load_path_code(path) {
 		/**
 		 * So what we do here, we check the PageHandlers for the directory path
-		 * and then we load any of them available, starting at the root level
-		 * and working downward.
+		 * and then we load the first one that's available, starting at the
+		 * bottom level and working upwards.
 		 *
 		 * They're automatically wrapped in closures and attached to the global
 		 * PageHandler object by Flask because they're in the scripts/pages
 		 * directory.
 		 */
+		var path_list = [];
 		var prev_paths = [];
 		path.forEach(function(p) {
 			prev_paths.push(p.toLowerCase());
-			var cur_path = prev_paths.join('/');
-			if (PageHandlers[cur_path]) {
-				PageHandlers[cur_path]();
-			}
+			path_list.push(prev_paths.join('/'))
 		});
+		path_list.reverse();
+		for (var p in path_list) {
+			if (PageHandlers[path_list[p]]) {
+				PageHandlers[path_list[p]]();
+				break;
+			}
+		}
 	}
 
 	init();
